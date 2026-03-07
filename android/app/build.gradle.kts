c52bb67a94a5cd7ceb638c3af9a833e0d24174b6
tatata10 <github79780942+tatata10@users.noreply.github.com>
2026-03-07 09:01:42 +0900

    feat: アプリアイコンの変更とランチャーアイコンの設定

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,13 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.sensuishi_app"
+    namespace = "jp.sennsuishi.lab"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,21 +22,34 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sensuishi_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "jp.sennsuishi.lab"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    val keystoreProperties = Properties()
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(keystorePropertiesFile.inputStream())
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            val storeFileName = keystoreProperties.getProperty("storeFile")
+            if (storeFileName != null) {
+                storeFile = file(storeFileName)
+            }
+            storePassword = keystoreProperties.getProperty("storePassword")
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/jp/sennsuishi/lab/MainActivity.kt
+++ android/app/src/main/kotlin/jp/sennsuishi/lab/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sensuishi_app
+package jp.sennsuishi.lab
 
 import io.flutter.embedding.android.FlutterActivity
 
